7ca0a8c27168f3865f0055f9b142b12b3bfa6941
Vraj <patelvraj119@gmail.com>
2023-02-06 16:23:26 +0000

    server file updated

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/collections/:collectionName',function (req,res,next){
       if(err){
           return next(err);
       }
-      res.send("Updated");
+      res.send(result);
   }));
 });
  
